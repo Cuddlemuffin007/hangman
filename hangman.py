4b6e2aea33a4872c832a9ba9a45df13687bdc555
Cuddlemuffin007 <brennonkrogers@gmail.com>
2016-02-04 13:52:09 -0500

    moved game logic into play_game function

--- hangman.py
+++ hangman.py
@@ -1,27 +1,41 @@
 # tiy hangman project
 # import random
 
-word = 'bathroom'
-
-hidden_word = list('_'*len(word))
-
-print(word, len(word))
-print(hidden_word, len(hidden_word))
-
 
 def get_user_guess():
     guess = input("Guess a letter.\n>> ").lower()
     return guess
 
-while ''.join(hidden_word) != word:
-    guess = get_user_guess()
 
-    for idx in range(len(word)):
-        if guess == word[idx]:
-            hidden_word[idx] = word[idx]
+def get_random_word():
+    pass
+
+
+def play_game():
+    turns = 5
+    word = 'bathroom'
+    hidden_word = list('_'*len(word))
+
+    while ''.join(hidden_word) != word and turns > 0:
+        guess = get_user_guess()
+
+        if guess not in word:
+            turns -= 1
+            print("Sorry your guess was incorrect. You have {} turns left.".format(turns))
+            if turns == 0:
+                print("Game over.")
+                break
+        elif guess in word:
+            for idx in range(len(word)):
+                if guess == word[idx]:
+                    hidden_word[idx] = word[idx]
 
     print(word)
     print(''.join(hidden_word))
+    exit(0)
 
-print(word)
-print(''.join(hidden_word))
+
+def play_again():
+    pass
+
+play_game()
